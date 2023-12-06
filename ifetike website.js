d20d1c8aa0cc119c25d32f6b15122431f67d5926
miracledaniel2005 <144464615+miracledaniel2005@users.noreply.github.com>
2023-12-06 16:42:32 +0100

    Add files via upload

--- ifetike website.js
+++ ifetike website.js
@@ -62,18 +62,17 @@ close11.addEventListener("click", function() {
 
 function reveal() {
     let reveals = document.querySelectorAll(".reveal");
-  
+
     for (var i = 0; i < reveals.length; i++) {
-      let windowHeight = window.innerHeight
-      let elementTop = reveals[i].getBoundingClientRect().top
-      let elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
+    let windowHeight = window.innerHeight
+    let elementTop = reveals[i].getBoundingClientRect().top
+    let elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active")
-      } else {
-        reveals[i].classList.remove("active")
-      }
+    } //else {
+    //     reveals[i].classList = ("active")
+    // }
     }
-  }
-  
-  window.addEventListener("scroll", reveal)
+}
+window.addEventListener("scroll", reveal)
